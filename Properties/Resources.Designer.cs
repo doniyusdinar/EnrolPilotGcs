9b825f84716c49e588d515960c5db3a354e3ae20
Doni Yusdinar <doni@xtremax.com>
2017-07-26 06:24:24 +0700

    add camera image to resources file

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -393,6 +393,16 @@ namespace MissionPlanner.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap camera_edit_kecil {
+            get {
+                object obj = ResourceManager.GetObject("camera_edit_kecil", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap camera_icon {
             get {
                 object obj = ResourceManager.GetObject("camera_icon", resourceCulture);
